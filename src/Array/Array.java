bf4a01726c4cc833d12d08e4cd35a5200c1ada0d
Scarlet Abreu <168682311+scarletabreu@users.noreply.github.com>
2024-10-03 15:35:32 -0400

    hole, terminamos

---
+++ src/Array/Array.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class Array {
+
+    public static void movNTimes(Object[] arr, int N){
+        for(int indext = 0; indext < N; indext++) {
+            for (int ind = arr.length / 2 - 1; ind > 0; ind--) {
+                Object temp = arr[ind];
+                arr[ind] = arr[ind-1];
+                arr[ind-1] = temp;
+            }
+            for (int ind = arr.length-1; ind > arr.length/2; ind--) {
+                Object temp = arr[ind];
+                arr[ind] = arr[ind-1];
+                arr[ind-1] = temp;
+            }
+        }
+    }
+
+    public static void printArray(Object[] arr){
+        for (Object object : arr) {
+            System.out.println(object);
+        }
+    }
+}
