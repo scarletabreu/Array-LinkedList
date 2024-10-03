189d1879516f14af4812f760fa7b6740ca453016
Underscore-IDPD <IDPD0001@ce.pucmm.edu.do>
2024-10-03 17:16:08 -0400

    Update Array.java

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -4,13 +4,17 @@ public class Array {
 
     public static void movNTimes(Object[] arr, int N){
         for(int indext = 0; indext < N; indext++) {
+            if(indext%2==0)
             for (int ind = arr.length / 2 - 1; ind > 0; ind--) {
                 Object temp = arr[ind];
                 arr[ind] = arr[ind-1];
                 arr[ind-1] = temp;
-                Object temp1 = arr[ind + arr.length/2];
-                arr[ind + arr.length/2] = arr[ind + arr.length/2 -1];
-                arr[ind + arr.length/2 - 1] = temp1;
+            }
+            else
+            for (int ind = arr.length - 1; ind > arr.length/2; ind--) {
+                Object temp = arr[ind];
+                arr[ind] = arr[ind-1];
+                arr[ind-1] = temp;
             }
         }
     }
